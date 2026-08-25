0d0563be4b12e26f6f817aebad8355365a6e9b8b
krievinsss <tagoritweb@gmail.com>
2026-08-25 09:52:40 +0300

    feat: add student DevCredits shop page

---
+++ app/shop/page.js
@@ -0,0 +1,6 @@
+import AppShell from '@/components/AppShell';
+import GamificationShop from '@/components/GamificationShop';
+import { requirePageUser } from '@/lib/page';
+import { getGamification,getTransactions,SHOP_ITEMS } from '@/services/gamification';
+
+export default async function ShopPage(){const user=await requirePageUser(['student']);const [profile,transactions]=await Promise.all([getGamification(user.id),getTransactions(user.id)]);return <AppShell user={user}><GamificationShop user={user} initialProfile={profile} initialTransactions={transactions} shop={SHOP_ITEMS}/></AppShell>}

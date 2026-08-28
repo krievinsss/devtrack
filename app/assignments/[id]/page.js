3516c38fdaf3961eb7fc812d3dc546f5440c62eb
krievinsss <tagoritweb@gmail.com>
2026-08-28 23:38:14 +0300

    feat: load formative and summative assessment streams

--- app/assignments/[id]/page.js
+++ app/assignments/[id]/page.js
@@ -7,17 +7,18 @@ import { getAssignment } from '@/services/assignments';
 import { getUsers } from '@/services/users';
 import { getProjects } from '@/services/projects';
 import { getFormativeEvents } from '@/services/formative';
+import { getSummativeEvents } from '@/services/summative';
 
 export default async function AssignmentPage({params}){
   const user=await requirePageUser(['teacher','admin']);
   const {id}=await params;
   const assignment=await getAssignment(id);
   if(!assignment)notFound();
-  const [groups,users,projects,events,commits,assessments]=await Promise.all([readJson('groups',[]),getUsers(),getProjects(),getFormativeEvents(id),readJson('commits',[]),readJson('assessments',[])]);
+  const [groups,users,projects,events,summatives,commits,assessments]=await Promise.all([readJson('groups',[]),getUsers(),getProjects(),getFormativeEvents(id),getSummativeEvents(id),readJson('commits',[]),readJson('assessments',[])]);
   const group=groups.find(g=>g.id===assignment.groupId);
   const students=(group?.studentIds||[]).map(x=>users.find(u=>u.id===x)).filter(Boolean);
   const linked=projects.filter(p=>p.assignmentId===id);
   const projectIds=new Set(linked.map(p=>p.id));
   const projectAssessments=assessments.filter(a=>projectIds.has(a.projectId));
-  return <AppShell user={user}><AssignmentDetail assignment={assignment} group={group} students={students} projects={linked} events={events} commits={commits} assessments={projectAssessments}/></AppShell>;
+  return <AppShell user={user}><AssignmentDetail assignment={assignment} group={group} students={students} projects={linked} events={events} summatives={summatives} commits={commits} assessments={projectAssessments}/></AppShell>;
 }
